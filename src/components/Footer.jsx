dd92127b8b4c8e90ac3e3c105187f169b3c7f817
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-11-30 15:19:44 +0100

    Feat: project setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/jsx-key */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import logo from "../assets/logo.png";
+import { BsFacebook, BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
+export default function Footer() {
+  const links = [
+    {
+      title: "About",
+      data: ["About", "Terms", "Legal"],
+    },
+    {
+      title: "NFT",
+      data: ["OpenSea", "Maker", "Learn"],
+    },
+    {
+      title: "Contact",
+      data: ["Press", "Support"],
+    },
+    {
+      title: "Social",
+      data: ["Twiiter", "Instagram"],
+    },
+  ];
+  const socialLink = [
+    // eslint-disable-next-line react/jsx-key
+    <BsFacebook />,
+    <BsTwitter />,
+    // eslint-disable-next-line react/jsx-key
+    <BsInstagram />,
+    <FaTiktok />,
+  ];
+  return (
+    <footer>
+      <div className="upper">
+        <div className="brand-container">
+          <div className="brand">
+            <img src={logo} alt="logo" />
+          </div>
+          <p>Exclusive NFT Collection</p>
+          <ul>
+            {socialLink.map((link, index) => (
+              <li key={index}>{link}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="links">
+          {links.map(({ title, data }, index) => {
+            return (
+              <div className="link" key={index}>
+                <h4>{title}</h4>
+                <ul>
+                  {data.map((link, index2) => (
+                    <li key={index2}>{link}</li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="lower">
+        <span>&copy; Copyright 2022 NFT</span>
+        <span>Launching August 2022</span>
+      </div>
+    </footer>
+  );
+}
